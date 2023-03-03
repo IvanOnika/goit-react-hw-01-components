6fb8dee3501a74ea6d686722ee6a3ae597a0f9de
Ivan Onika <vanyaonika@gmail.com>
2023-03-03 15:07:23 +0200

    Transaction done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,9 @@ import stats from '../components/statistics/data.json';
 import { FriendList } from 'components/friendList/FriendList';
 import friends from '../components/friendList/friends.json';
 
+import transactions from 'components/transactionHistory/transactions.json';
+import { TransactionHistory } from '../components/transactionHistory/TransactionHistory';
+
 export const App = () => {
   return (
     <>
@@ -17,10 +20,10 @@ export const App = () => {
         stats={user.stats}
       />
       <Statistics title="Upload stats" stats={stats} />
-      
-      <FriendList friends={friends} />;
 
+      <FriendList friends={friends} />;
       
+      <TransactionHistory items={transactions} />;
     </>
   );
 };
