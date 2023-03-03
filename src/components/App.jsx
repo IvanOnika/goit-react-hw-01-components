55408bef3d81210b29cea5e1997e4ce47dc3eb24
Ivan Onika <vanyaonika@gmail.com>
2023-03-03 14:52:00 +0200

    Friend list done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,9 @@ import user from '../components/profile/user.json';
 import { Statistics } from 'components/statistics/Statistics';
 import stats from '../components/statistics/data.json';
 
+import { FriendList } from 'components/friendList/FriendList';
+import friends from '../components/friendList/friends.json';
+
 export const App = () => {
   return (
     <>
@@ -13,8 +16,11 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-
       <Statistics title="Upload stats" stats={stats} />
+      
+      <FriendList friends={friends} />;
+
+      
     </>
   );
 };
