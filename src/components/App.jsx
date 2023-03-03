0fa14f6a2dcd9aa306c79c4d73e242d214321a19
Ivan Onika <vanyaonika@gmail.com>
2023-03-03 14:36:53 +0200

    statistics done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,20 @@
 import { Profile } from 'components/profile/Profile';
 import user from '../components/profile/user.json';
 
+import { Statistics } from 'components/statistics/Statistics';
+import stats from '../components/statistics/data.json';
+
 export const App = () => {
   return (
-    <Profile
-
+    <>
+      <Profile
         tag={user.tag}
         location={user.location}
         avatar={user.avatar}
         stats={user.stats}
       />
+
+      <Statistics title="Upload stats" stats={stats} />
+    </>
   );
 };
