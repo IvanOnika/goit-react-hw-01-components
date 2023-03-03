533a67aeed1fd6a3e4b96bd7456502f383728d47
Ivan Onika <vanyaonika@gmail.com>
2023-03-03 14:19:07 +0200

    profile done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,14 @@
+import { Profile } from 'components/profile/Profile';
+import user from '../components/profile/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Profile
+
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
   );
 };
